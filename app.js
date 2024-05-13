b1b2594d30042d753db561ce71b9a0e6763426a6
AtaiBerdigulov <BerdigulovAtai@gmail.com>
2024-05-13 10:37:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// цикл do, while
+
+// let water=0
+
+// do {
+//     console.log('1 litr suu kuiuldo', water);
+//     water = water + 1;
+// } while (water < 10);
+
+// let userName
+// do{
+//     userName=prompt('What is your name?')
+// }while (userName === '');
+// console.log(userName);
+
+// let number=1
+
+// while(number<6){
+//     console.log('bul san', number);
+//     number++;
+// }
+// for (i=1; i <6; i++){
+//     console.log(('Bul san', number));
+// }
+
+// const drinks=['water', 'cola', 'pepsi', 'fanta']
+
+// for (i=0; i<drinks.length; i++){
+//     console.log('Bugunku suusunduk', drinks[i]);
+// }
+
+// const sandar=[]
+
+// for (i=0; i<5; i++){
+//     const san=prompt('sab jaz')
+//     sandar.push(san)
+// }
+// console.log(sandar);
+
+// const san=Math.random()*5;
+// console.log(san);
+// console.log(Math.floor(san));
+
+// console.log(Math.pow(8,4));
+// console.log(Math.max(16,7));
+// console.log(Math.min(6,3));
+
+const drinks=['mercedec', 'bmw', 'audi', 'lambargini']
+
+for (i=0; i<drinks.length; i++){
+    console.log('jakshi korgon mashina', drinks[i]);
+}
